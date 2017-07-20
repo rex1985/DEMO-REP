8d1a0963546034e01654631825f78628d8100b63
Admin <Admin@Admin-PC>
2017-07-20 19:03:33 +0530

    new git demo

--- DEMOGIT/src/test/GitDemo2.java
+++ DEMOGIT/src/test/GitDemo2.java
@@ -2,9 +2,9 @@ package test;
 
 public class GitDemo2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+System.out.println("git demo2");
 	}
 
 }
